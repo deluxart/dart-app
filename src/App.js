3d51806f0acc6b3efcbd800e068928fafdcb1b38
deluxart <deluxart3@gmail.com>
2021-02-03 15:53:52 +0200

    Add modules / eslint

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Header from './modules/look/Header';
 import history from './utils/history';
-import PortfolioPage from './modules/pages/portfolio';
-import ErrorPage from './modules/pages/404';
+import Layout from './modules/layout';
 
 const isBrowserSupportsHistory = 'pushState' in window.history;
 
 function App() {
   return (
       <Router history={history} forceRefresh={!isBrowserSupportsHistory}>
-        <div className="App">
-            <Header text="Hello world!"/>
-            <Switch>
-                <Route path="/" render={() => <h1>Hello</h1>} exact/>
-                <Route path="/portfolio" component={PortfolioPage} exact/>
-                <Route path="/portfolio/:id" render={({ match }) => {
-                    const { id } = match.params;
-                    return (
-                        <p>Page {id}</p>
-                    );
-                }} />
-                <Route component={ErrorPage} />
-            </Switch>
-        </div>
+        <Layout />
       </Router>
   );
 }
