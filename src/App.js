ab9d75556625495662d8acf4c5330451b77d3d5e
deluxart <deluxart3@gmail.com>
2021-02-03 15:46:18 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './modules/look/Header';
+import history from './utils/history';
+import PortfolioPage from './modules/pages/portfolio';
+import ErrorPage from './modules/pages/404';
+
+const isBrowserSupportsHistory = 'pushState' in window.history;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router history={history} forceRefresh={!isBrowserSupportsHistory}>
+        <div className="App">
+            <Header text="Hello world!"/>
+            <Switch>
+                <Route path="/" render={() => <h1>Hello</h1>} exact/>
+                <Route path="/portfolio" component={PortfolioPage} exact/>
+                <Route path="/portfolio/:id" render={({ match }) => {
+                    const { id } = match.params;
+                    return (
+                        <p>Page {id}</p>
+                    );
+                }} />
+                <Route component={ErrorPage} />
+            </Switch>
+        </div>
+      </Router>
   );
 }
 
